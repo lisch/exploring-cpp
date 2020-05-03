bdae97e06af0d77a31ebfc1d72bc77b46bca0fdb
Ray Lischner <ray@klipp-lischner.net>
2020-05-03 00:11:25 -0400

    A few changes to work better on Windows.

--- code/chap09/snip0902.cpp
+++ code/chap09/snip0902.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 int main() {
 std::vector<int> data{ 10, 20, 30 };
+return
 #include "snip0902.hh"
 ;
 }
--- code/chap52/snip5204.cpp
+++ code/chap52/snip5204.cpp
@@ -1,5 +1,5 @@
 #include <vector>
 int main() {
 #include "snip5204.hh"
-  return matrix.size();
+  return matrix.size() != 0;
 }
